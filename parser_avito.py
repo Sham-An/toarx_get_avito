b3bbdffa28147dc65a60da1f96516f7e851e2c01
miladmin <shamraev@inbox.ru>
2022-09-01 19:09:21 +0300

    parser_avito.py OK!

--- parser_avito.py
+++ parser_avito.py
@@ -5,6 +5,39 @@ from datetime import datetime
 import requests
 from realty import check_database
 
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.poolmanager import PoolManager
+from requests.packages.urllib3.util import ssl_
+from datetime import datetime
+from realty import check_database
+
+import ssl
+import requests
+
+
+
+CIPHERS = """ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-SHA256:AES256-SHA"""
+
+class TlsAdapter(HTTPAdapter):
+
+    def __init__(self, ssl_options=0, **kwargs):
+        self.ssl_options = ssl_options
+        super(TlsAdapter, self).__init__(**kwargs)
+
+    def init_poolmanager(self, *pool_args, **pool_kwargs):
+        ctx = ssl_.create_urllib3_context(ciphers=CIPHERS, cert_reqs=ssl.CERT_REQUIRED, options=self.ssl_options)
+        self.poolmanager = PoolManager(*pool_args, ssl_context=ctx, **pool_kwargs)
+
+# session = requests.session()
+# adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+# session.mount("https://", adapter)
+#
+# try:
+#     r = session.request('GET', 'https://www.avito.ru')
+#     print(r.text)[1000]
+# except Exception as exception:
+#     print(exception)
+
 
 def get_offer(item):
     offer = {}
@@ -62,8 +95,20 @@ def get_json():
     )
 
     url = 'https://m.avito.ru/api/11/items'
+    #url = 'https://m.avito.ru/api/11/items?key=af0deccbgcgidddjgnvljitntccdduijhdinfgjgfjir&categoryId=9'  # &params[1283]D=14756'#&locationId=640000&params%5B110000%5D=329273&withImagesOnly=1&page=1' #&lastStamp=1611316560&display=list&limit=30'
 
-    response = requests.get(url=url, headers=headers, params=params)
+    session = requests.session()
+    adapter = TlsAdapter(ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1)
+    session.mount("https://", adapter)
+
+    try:
+        response = session.request('GET', url, headers=headers, params=params)
+        #r = session.request('GET', 'https://www.avito.ru')
+        print(response.text)[1000]
+    except Exception as exception:
+        print(exception)
+
+    #response = requests.get(url=url, headers=headers, params=params)
     data = response.json()
 
     return data
